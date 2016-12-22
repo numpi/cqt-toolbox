4f301cdab98093e5ce4392ea7f0c178f7c6604e4
Stefano Massei <stefano.massei@sns.it>
2016-12-22 12:19:45 +0100

    added norm and fixed some cases in plus and mtimes

--- @cqt/cqt.m
+++ @cqt/cqt.m
@@ -1,4 +1,14 @@
 classdef cqt 
+%CQT  class of continuosly quasi-Toeplitz matrices
+%
+%     T = CQT(pos, neg, U, V) creates the CQT-matrix with the specified symbol
+%     and finite correction U * V.'
+%
+%     T = CQT(pos, neg) creates the CQT-matrix with the specified symbol and
+%     an empty finite correction
+%
+%     T = CQT(A) creates the CQT-matrix with a symbol equal to 0 and finite
+%     correction equal to A
     properties
         % Vector containing the nonnegative coefficients of the symbol
         p
@@ -17,11 +27,31 @@ classdef cqt
 
         function obj = cqt(varargin)
         %CQT Create a a QuasiToeplitz matrix in the Wiener class
-            obj.n = varargin{1};
-            obj.p = varargin{2};
-            obj.U = varargin{3};
-            obj.V = varargin{4};
-        end
+		if length(varargin) == 4
+	    		if varargin{1}(1) ~= varargin{2}(1)
+				error('The coefficients of degree 0 does not coincide');
+	    		end
+	    		if size(varargin{3},2) == size(varargin{4},2)
+            			obj.n = varargin{1};
+            			obj.p = varargin{2};
+            			obj.U = varargin{3};
+            			obj.V = varargin{4};
+	    		else
+				error('Incompatible dimensions: the last two arguments must have the same number of columns');
+	   		end
+		elseif length(varargin) == 1
+	    		obj.n = 0;
+            		obj.p = 0;
+            		obj.U = varargin{1};
+            		obj.V = eye(size(varargin{1},2));
+		elseif length(varargin) == 2
+			if varargin{1}(1) ~= varargin{2}(1)
+				error('The coefficients of degree 0 does not coincide');
+	    		end
+	    		obj.n = varargin{1};
+            		obj.p = varargin{2};
+        	end
 
-    end
+    	end
+     end
 end
--- @cqt/mtimes.m
+++ @cqt/mtimes.m
@@ -1,20 +1,24 @@
 function [ T ] = mtimes(T1, T2)
 %MTIMES Multiply two CQT matrices T1 and T2. 
 %
-% T = MTIMES(T1, T2) computes the CQT matrix T obtained multiplying two CQT
+%     T = MTIMES(T1, T2) computes the CQT matrix T obtained multiplying two CQT
 %     matrices T1 and T2. If T2 is a dense finite matrix it is considered
 %     as a semiinfinite matrix with only the leading top-left corner
 %     different from zero. 
-
-if isa(T2, 'cqt')
-    [cm,cp,cU,cV]=qt_mult(T1.n, T1.p, T1.U, T1.V, T2.n, T2.p, T2.U, T2.V);
-    T = cqt(cm, cp, cU, cV);
+if isa(T1,'cqt')
+	if isa(T2, 'cqt')
+    		[cm,cp,cU,cV]=qt_mult(T1.n, T1.p, T1.U, T1.V, T2.n, T2.p, T2.U, T2.V);
+    		T = cqt(cm, cp, cU, cV);
+	elseif isscalar(T2)
+    		T = cqt(T1.n * T2, T1.p * T2, T1.U * T2, T1.V);
+	else
+		error('Incompatible types multiplication. \nIf you want to multiply a cqt matrix T with a finite matrix of %s A you can use T * cqt(A) ',class(T2));
+	end
 else
-    [cm,cp,cU,cV]=qt_mult(T1.n, T1.p, T1.U, T1.V, ...
-        0, 0, T2, eye(size(T2,2)));
-    T = cqt(cm, cp, cU, cV);
-end
-
-
+	if isscalar(T1) && isa(T2, 'cqt')
+		T = cqt(T2.n * T1, T2.p * T1, T2.U * T1, T2.V);
+	else
+		error('Incompatible types multiplication. \nIf you want to multiply a finite matrix of %s A with a cqt matrix you can use cqt(A) * T',class(T1));
+	end
 end
 
