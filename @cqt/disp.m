27fdd38843feb0cd2a35e78af2cc2e64f7f44ae3
Leonardo Robol <leo@robol.it>
2016-12-23 10:21:10 +0100

    Updated @cqt/disp.m to display the new finite CQTs.

--- @cqt/disp.m
+++ @cqt/disp.m
@@ -1,11 +1,25 @@
 function disp(T)
 %DISP Display a CQT matrix on the screen. 
 
-fprintf('CQT Matrix\n\n');
-fprintf(' - Toeplitz part: \n\n');
-disp(toeplitz([ T.n 0 0], [ T.p 0 0 ]));
-fprintf('\n - Finite part (top-left corner): \n\n');
-disp(T.U * T.V.');
+fprintf('CQT Matrix of size %d x %d\n\n', T.sz(1), T.sz(2));
+
+% Check if the CQT has a non-zero Toeplitz part
+if length(T.n) + length(T.p) > 0
+    fprintf(' - Toeplitz part (leading %d x %d block): \n', ...
+        length(T.n) + 2, length(T.p) + 2);
+    disp(toeplitz([ T.n 0 0], [ T.p 0 0 ]));
+end
+
+if size(T.U, 1) > 0 && size(T.V, 2) > 0
+    fprintf('\n - Finite correction (top-left corner): \n');
+    disp(T.U * T.V.');
+end
+
+if size(T.W, 1) > 0 && size(T.Z, 2) > 0
+    fprintf('\n - Finite correction (bottom-right corner): \n');
+    S = T.W * T.Z';
+    disp(S(end:-1:1,end:-1:1));
+end
 
 end
 
