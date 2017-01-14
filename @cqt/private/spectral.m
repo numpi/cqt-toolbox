610022d7c6e1fb9ca9bcffba1cf9bac3fd75da28
Leonardo Robol <leo@robol.it>
2017-01-14 18:29:42 +0100

    Removed deprecated use of spectral_cr.

--- @cqt/private/spectral.m
+++ @cqt/private/spectral.m
@@ -3,11 +3,7 @@ function [l, u, w, n] = spectral(vm, vp)
 
 switch cqtoption('inversion')
 	case 'cr'
-		if nargout > 2
-			[l, u, w, n] = spectral_cr(vm, vp);
-		else
-			[l, u] = spectral_cr(vm, vp);
-		end
+		[l, u] = spectral_cr(vm, vp);		
 	case 'fft'
 		[l, u] = spectral_fft(vm, vp);
 end
