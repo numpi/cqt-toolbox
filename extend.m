963f0adb1e51332925395b1478be3d12dba268b1
Leonardo Robol <leo@robol.it>
2017-11-13 14:41:33 +0100

    Added check on usage of extend.

--- extend.m
+++ extend.m
@@ -1,7 +1,11 @@
 function A = extend(A, c)
 %EXTEND Add a rank-1 e * c' correction to A, where e = ones(inf,1).
 %
-% If the correction has already been set, the new value of C replaces it. 
+% If the correction has already been set, the new value of C replaces it.
+
+if min(size(A)) ~= inf
+    error('extended Toeplitz arithmetic is only supported for infinite matrices');
+end
 
 A.c = c;
 
