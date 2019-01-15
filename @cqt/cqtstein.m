5cbb82b3758fd0585212fbbb32e2a10550cdacdb
Leonardo Robol <leo@robol.it>
2019-01-15 16:58:49 +0100

    Fixed signs.

--- @cqt/cqtstein.m
+++ @cqt/cqtstein.m
@@ -1,18 +1,19 @@
-function X = cqtstein(A, B, C, poles)
+function X = cqtstein(A, B, C)
 %CQTSTEIN Solves the equation A*X*B + X = C
 %
 % X = CQTSTEIN(A, B, C) computes the CQT solution to the matrix equation 
 %
-%    (1)   A*X*B + X = C 
+%    (1)   A*X*B + X + C = 0
 %
 %     under the assumption that both A and B have spectral radius strictly
 %     smaller than 1. 
 
-I = cqt(1,1);
+IA = eye(size(A), 'like', A);
+IB = eye(size(B), 'like', B);
 
-M1 = inv(A - I);
-M2 = inv(B + I);
+M1 = inv(A - IA);
+M2 = inv(B + IB);
 
-X = cqtlyap(M1 * (A + I), M2 * (B - I), 2 * M1 * C * M2);
+X = cqtlyap(M1 * (A + IA), M2 * (B - IB), 2 * M1 * C * M2);
 
 end
