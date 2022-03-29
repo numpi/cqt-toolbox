9a9684166a162612513a67a2a68da4e704844e6b
Leonardo Robol <leo@robol.it>
2022-03-29 08:54:03 +0200

    Fix documentation for distances.

--- docs/cmd_distances.m
+++ docs/cmd_distances.m
@@ -1,16 +1,16 @@
 %% Distances from truncated matrices
 %
 % Given an infinite size QT matrix $A$, its finite truncation may (or may
-% not) well approximate the spectrum of the infinite-dimensional operator. 
+% not) well approximate the spectrum of the infinite-dimensional operator.
 %
 % The function |distance| computes the distance between the eigenvalues of
 % the truncated matrix and the closest eigenvalue of the
-% infinite-dimensional operator. 
+% infinite-dimensional operator.
 
 %% Syntax
 %
-% |[D, x] = distances(A, algo, N0, nsamp, advpx, dig)|
+% |[D, x] = distances(A, x, N0, nsamp, advpx, dig)|
 
 %% Example
 % See <doc_eig.html the documentation on computing eigenvalues> for further
-% details on the use of this function. 
+% details on the use of this function.
